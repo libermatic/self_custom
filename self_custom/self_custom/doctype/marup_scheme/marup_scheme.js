41f167590e413e380e5a1dfb80e2948ce1539d38
Sun Howwrongbum <sun@libermatic.com>
2022-07-20 04:48:03 +0000

    feat: can disable MarupPeriod

--- self_custom/self_custom/doctype/marup_scheme/marup_scheme.js
+++ self_custom/self_custom/doctype/marup_scheme/marup_scheme.js
@@ -25,4 +25,35 @@ frappe.ui.form.on('Marup Scheme', {
       return { filters: { is_group: 0, company } };
     });
   },
+  refresh: function (frm) {
+    if (!frm.doc.__islocal && frm.doc.status != 'Completed') {
+      frm.add_custom_button('Suspend Period', function () {
+        const dialog = new frappe.ui.Dialog({
+          title: 'Disable a Marup Period',
+          fields: [
+            {
+              label: 'Marup Period',
+              fieldtype: 'Link',
+              fieldname: 'period',
+              options: 'Marup Period',
+              get_query: () => ({
+                filters: { marup_scheme: frm.doc.name, disabled: 0 },
+              }),
+              only_select: 1,
+              reqd: 1,
+            },
+          ],
+        });
+
+        dialog.set_primary_action('OK', async function () {
+          const args = dialog.get_values();
+          await frm.call('disable_period', args);
+          frm.reload_doc();
+          dialog.hide();
+        });
+        dialog.onhide = () => dialog.$wrapper.remove();
+        dialog.show();
+      });
+    }
+  },
 });

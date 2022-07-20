6996ae87b67060fb6654e5b719163c788bdc1d5f
Sun Howwrongbum <sun@libermatic.com>
2022-07-20 07:46:01 +0000

    feat: handle JournalEntrys

--- self_custom/self_custom/doctype/marup_subscription/marup_subscription.py
+++ self_custom/self_custom/doctype/marup_subscription/marup_subscription.py
@@ -1,8 +1,16 @@
 # Copyright (c) 2022, Libermatic and contributors
 # For license information, please see license.txt
 
+from typing import Tuple
 import frappe
 from frappe.model.document import Document
+
+from self_custom.doc_events.journal_entry import (
+    COMMISSION_ENTRY,
+    MARUP_VOUCHER_TYPES,
+    SUBSCRIPTION_ENTRY,
+)
+
 
 class MarupSubscription(Document):
     def validate(self):
@@ -34,3 +42,130 @@ class MarupSubscription(Document):
                 "<strong>Marup Period</strong>."
             )
 
+    def on_submit(self):
+        share = frappe.get_doc("Marup Share", self.marup_share)
+        scheme = frappe.get_doc("Marup Scheme", self.marup_scheme)
+        settings = frappe.db.get_value(
+            "Marup Default", filters={"company": scheme.company}, fieldname="*"
+        )
+        company = frappe.get_doc("Company", scheme.company)
+        args = [self, share, scheme, settings, company]
+
+        subscription_entry = make_journal_entry(*[SUBSCRIPTION_ENTRY, *args])
+        subscription_entry.flags.ignore_permissions = True
+        subscription_entry.insert()
+        subscription_entry.submit()
+
+        commission_entry = make_journal_entry(*[COMMISSION_ENTRY, *args])
+        commission_entry.flags.ignore_permissions = True
+        commission_entry.insert()
+        commission_entry.submit()
+
+    def before_cancel(self):
+        for (name,) in frappe.get_all(
+            "Journal Entry",
+            filters={
+                "voucher_type": ("in", MARUP_VOUCHER_TYPES),
+                "docstatus": 1,
+                "marup_subscription": self.name,
+            },
+            as_list=1,
+        ):
+            je = frappe.get_doc("Journal Entry", name)
+            je.flags.ignore_permissions = True
+            je.cancel()
+
+
+def make_journal_entry(
+    voucher_type: str, subscription, share, scheme, settings, company
+):
+    def get_accounts() -> Tuple[str, str]:
+        if voucher_type == SUBSCRIPTION_ENTRY:
+            return (
+                scheme.member_account
+                or settings.default_member_account
+                or company.default_receivable_account,
+                scheme.subscription_account
+                or settings.default_subscription_account
+                or company.default_income_account,
+            )
+
+        if voucher_type == COMMISSION_ENTRY:
+            return (
+                scheme.partner_account
+                or settings.default_partner_account
+                or company.default_payable_account,
+                scheme.commission_account
+                or settings.default_commission_account
+                or company.default_expense_account,
+            )
+
+    def get_party() -> Tuple[str, str]:
+        if voucher_type == SUBSCRIPTION_ENTRY:
+            return "Customer", share.customer
+
+        if voucher_type == COMMISSION_ENTRY:
+            return "Sales Partner", share.sales_partner
+
+    def get_amounts() -> Tuple[float, float]:
+        if voucher_type == SUBSCRIPTION_ENTRY:
+            return scheme.unit_price, 0
+
+        if voucher_type == COMMISSION_ENTRY:
+            return 0, scheme.unit_price * share.commission_rate / 100.0
+
+    def get_title():
+        if voucher_type == SUBSCRIPTION_ENTRY:
+            return share.customer_name
+
+        if voucher_type == COMMISSION_ENTRY:
+            return share.sales_partner_name
+
+    def postprocess(source, target):
+        target.update(
+            {
+                "voucher_type": voucher_type,
+                "company": scheme.company,
+                "title": get_title(),
+            }
+        )
+
+        party_account, against_account = get_accounts()
+        party_type, party = get_party()
+        debit_amount, credit_amount = get_amounts()
+        target.set(
+            "accounts",
+            [
+                {
+                    "account": party_account,
+                    "debit_in_account_currency": debit_amount,
+                    "credit_in_account_currency": credit_amount,
+                    "party_type": party_type,
+                    "party": party,
+                    "marup_scheme": scheme.name,
+                },
+                {
+                    "account": against_account,
+                    "debit_in_account_currency": credit_amount,
+                    "credit_in_account_currency": debit_amount,
+                    "cost_center": scheme.cost_center or company.cost_center,
+                    "marup_scheme": scheme.name,
+                },
+            ],
+        )
+
+    return frappe.model.mapper.get_mapped_doc(
+        "Marup Subscription",
+        subscription.name,
+        {
+            "Marup Subscription": {
+                "doctype": "Journal Entry",
+                "field_map": {
+                    "posting_date": "posting_date",
+                    "name": "marup_subscription",
+                },
+            }
+        },
+        None,
+        postprocess,
+    )

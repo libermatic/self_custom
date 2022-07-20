9c1c373ed40cf8d3aa26f054b4022c994b1438e6
Sun Howwrongbum <sun@libermatic.com>
2022-07-20 06:02:15 +0000

    feat: copy default partner from Customer

--- self_custom/self_custom/doctype/marup_share/marup_share.js
+++ self_custom/self_custom/doctype/marup_share/marup_share.js
@@ -4,5 +4,24 @@
 frappe.ui.form.on('Marup Share', {
 	// refresh: function(frm) {
 
-	// }
+  customer: async function (frm) {
+    const { customer } = frm.doc;
+    if (customer) {
+      const {
+        message: { default_sales_partner, default_commission_rate } = {},
+      } = await frappe.db.get_value('Customer', customer, [
+        'default_sales_partner',
+        'default_commission_rate',
+      ]);
+      frm.set_value({
+        sales_partner: default_sales_partner,
+        commission_rate: default_commission_rate,
+      });
+    } else {
+      frm.set_value({
+        sales_partner: null,
+        commission_rate: null,
+      });
+    }
+  },
 });

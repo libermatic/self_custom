1453ecfee41b4ef024bac2fe1d82d334488bea78
Sun Howwrongbum <sun@libermatic.com>
2022-10-06 05:50:16 +0000

    refactor: use pyproject

--- setup.py
+++ setup.py
@@ -1,20 +1,6 @@
-# -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+# TODO: Remove this file when bench >=v5.11.0 is adopted / v15.0.0 is released
+from setuptools import setup
 
-# get version from __version__ variable in self_custom/__init__.py
-from self_custom import __version__
+name = "self_custom"
 
-with open("requirements.txt") as f:
-    install_requires = f.read().strip().split("\n")
-
-setup(
-    name="self_custom",
-    version=__version__,
-    description="Customizations for SELF",
-    author="Libermatic",
-    author_email="info@libermatic.com",
-    packages=find_packages(),
-    zip_safe=False,
-    include_package_data=True,
-    install_requires=install_requires,
-)
+setup()
